e342abe2d8b96fdd98eb51c6dc786ebca0604e15
Glisten **STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2021-04-13 17:46:54 -0700

    change the db configuration with mongodb atlas

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://danil:**lionheart**!@cluster0.bpdyq.mongodb.net",//your cloud mongodb uri write here  
+  mongoURI: "mongodb+srv://danil:**lionheart**!@cluster0.bpdyq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",//your cloud mongodb uri write here  
     secretOrKey: "PBFPVYFBQXZTYFPBFEQJHDXXQVAPTPQJKTOYQWIPBVWLXTOXBTFXQ!^#WAXBVCXQWAXFQJVWLEQNTOZQGGQLFXQWAKVWLXQWAEBIPBFXF!QVXGTVJVWLBTPQWAEBFPBFHCVLXBQUFEVWLXGDP"
   };
